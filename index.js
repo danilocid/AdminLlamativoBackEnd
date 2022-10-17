6d9f915926011de668f1c271d1a4815fed4bd349
danilo-cid <danilo.cid@zentagroup.com>
2022-10-17 11:09:27 -0300

    test .env

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 const Server = require("http").createServer(app);
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+require("dotenv").config();
 
 Server.listen(8080, () => {
   console.clear();
   console.log("Server is running on port 8080");
+  console.log(process.env.JWT_SECRET);
 });
 app.use(cors());
 app.use(express.json());
